47a05c003430382f0f25ac8a4ddc3b43c4497785
Alejandro <vaquerolopez.alejandro@gmail.com>
2025-02-19 12:07:40 +0000

    Developed basic cpp implementation

--- lua/commands.lua
+++ lua/commands.lua
@@ -46,9 +46,7 @@ function M.get_class_info()
 	local bufnr = vim.api.nvim_get_current_buf()
 	local parser = vim.treesitter.get_parser(bufnr)
 	local tree = parser:parse()[1]
-	local ts_utils = require("nvim-treesitter.ts_utils")
 
-	-- Navegar por los nodos del árbol para encontrar namespaces
 	local root = tree:root()
 
 	local file_structure = {
@@ -66,10 +64,11 @@ function M.get_class_info()
 	file_structure.namespaces = M.get_namespaces()
 
 	local queryString =
-		"(class_specifier name:(type_identifier)@className body:(field_declaration_list (declaration declarator: (function_declarator declarator: (destructor_name)@destructor ))))(class_specifier name:(type_identifier)@className body:(field_declaration_list (declaration declarator: (function_declarator declarator: (identifier) parameters: (parameter_list))@parameterList)))"
+		"(class_specifier name: (type_identifier) @className) (class_specifier body: (field_declaration_list (declaration declarator: (function_declarator declarator: (destructor_name) @destructor)))) (class_specifier body: (field_declaration_list (declaration declarator: (function_declarator declarator: (identifier) parameters: (parameter_list)) @parameterList))) (class_specifier name: (type_identifier) body: (field_declaration_list (field_declaration type: [(qualified_identifier) (primitive_type)] @type declarator: (function_declarator) @methodName)))"
 
 	local result = vim.treesitter.query.parse("cpp", queryString)
 	local actual_class = ""
+	local method_combination_counter = 0
 	for id, node in result:iter_captures(root, bufnr) do
 		local capture_name = result.captures[id]
 		local text_name = vim.treesitter.get_node_text(node, bufnr)
@@ -94,22 +93,22 @@ function M.get_class_info()
 				file_structure.classes[actual_class].needDestructor = true
 			end
 		end
+
+		if capture_name == "type" and actual_class ~= nil then
+			if file_structure.classes[actual_class] and method_combination_counter == 0 then
+				table.insert(file_structure.classes[actual_class].methods, { type = text_name, name = "" })
+				method_combination_counter = method_combination_counter + 1
+			end
+		end
+
+		if capture_name == "methodName" and actual_class ~= nil then
+			if file_structure.classes[actual_class] and method_combination_counter == 1 then
+				file_structure.classes[actual_class].methods[#file_structure.classes[actual_class].methods].name =
+					text_name
+				method_combination_counter = 0
+			end
+		end
 	end
-
-	local results = {}
-	for id, node, _ in result:iter_captures(root, bufnr, 0, -1) do
-		local capture_name = result.captures[id] -- Nombre de la captura en la query
-		local node_text = vim.treesitter.get_node_text(node, bufnr) -- Texto del nodo
-
-		table.insert(results, capture_name .. " " .. node_text) -- Agrega el texto capturado
-	end
-
-	local file = io.open(vim.fn.expand("test.txt"), "w") -- Guardar en home
-	if file then
-		file:write(table.concat(results, "\n"))
-		file:close()
-	end
-
 	return file_structure
 end
 
@@ -133,14 +132,20 @@ function M.generate_cpp_file()
 		table.insert(cpp_lines, "namespace " .. ns .. " {")
 	end
 
+	table.insert(cpp_lines, "\n")
+
 	for _, value in pairs(file_structure.classes) do
 		for _, each in ipairs(value.constructors) do
 			table.insert(cpp_lines, value.name .. "::" .. each .. "{")
-			table.insert(cpp_lines, "}")
+			table.insert(cpp_lines, "}\n")
 		end
 		if value.needDestructor then
 			table.insert(cpp_lines, value.name .. "::" .. "~" .. value.name .. "()" .. "{")
-			table.insert(cpp_lines, "}")
+			table.insert(cpp_lines, "}\n")
+		end
+		for _, method in ipairs(value.methods) do
+			table.insert(cpp_lines, method.type .. " " .. value.name .. "::" .. method.name .. "{")
+			table.insert(cpp_lines, "}\n")
 		end
 	end
 
@@ -148,12 +153,10 @@ function M.generate_cpp_file()
 		table.insert(cpp_lines, "}")
 	end
 
-	-- Escribir el archivo .cpp
 	local file = io.open(cpp_filename, "w")
 	if file then
 		file:write(table.concat(cpp_lines, "\n"))
 		file:close()
-		-- print("Archivo generado: " .. cpp_filename)
 		vim.cmd("edit " .. cpp_filename)
 	else
 		print("Error al crear el archivo " .. cpp_filename)
