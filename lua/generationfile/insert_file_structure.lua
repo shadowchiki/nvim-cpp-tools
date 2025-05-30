a48efacd59dc08d6500c9048967c0abd6590d6ad
Alejandro <vaquerolopez.alejandro@gmail.com>
2025-05-30 16:39:52 +0000

    Fixed params with references

--- lua/generationfile/insert_file_structure.lua
+++ lua/generationfile/insert_file_structure.lua
@@ -81,7 +81,7 @@ function M.insert_method(actual_class, capture_name, capture_value, start_row, e
 	if capture_name == "methodName" and actual_class ~= nil then
 		if actual_class then
 			capture_value = string.gsub(capture_value, "override", "")
-			if string.find(capture_value, "&") then
+			if string.sub(capture_value, 1, 1) == "&" then
 				capture_value = string.gsub(capture_value, "&", "")
 				actual_class.methods[#actual_class.methods].type = actual_class.methods[#actual_class.methods].type
 					.. "&"
